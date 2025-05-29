0b17f7b701cb10c7d7f495f5812b8c8ec4b74db7
anurag <016.krsna@gmail.com>
2025-05-29 07:42:18 +0530

    Initial Commit

---
+++ _02_arrays/arrayDeclaration.java
@@ -0,0 +1,9 @@
+package _02_arrays;
+
+class arrayDeclaration {
+    public static void main(String[] args) {
+        // In this way we can declare an arraya
+        int arr[];
+        int[] arr1;  // Most Preferable
+    }
+}
---
+++ _02_arrays/arrayInitialization.java
@@ -0,0 +1,12 @@
+package _02_arrays;
+
+public class arrayInitialization {
+    public static void main(String[] args) {
+        int[] arr; // Declaration of Array
+        arr = new int[10]; // Initialization of an array
+        // We can do both the steps in one go:
+        int[] arr1 = new int[10];
+        // We can also directly pass the elements as well
+        int[] arr2 = {1,2,3,4,5};
+    }    
+}
